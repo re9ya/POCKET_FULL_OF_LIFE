9d3287e17225245735440424e036a7deed57d5ae
R3iYaj <rei.huabci.yang@gmail.com>
2026-01-03 23:22:06 -0600

    script for S2

---
+++ SCENE2/s2-script.js
@@ -0,0 +1,17 @@
+currentScript = [
+    {
+        bg: 'Images/BG-HomeEnterance.png',
+        name: "Tico",
+        text: "Hello...!"
+    },
+    {
+        bg: 'Images/BG-HomeEnterance.png',
+        name: "Tico",
+        text: "My name is Tico, but you could also call me Ti."
+    }
+];
+
+// Engine Start
+window.onload = () => {
+    renderLine();
+};
